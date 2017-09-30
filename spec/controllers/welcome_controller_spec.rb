2b42be32a415a634c17cb87790861ee8e27b25d0
William Van Deren <wvanderen@gmail.com>
2017-09-30 14:50:48 +0000

    checkpoint12-assigment

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
     describe "GET index" do
         it "renders the index template" do
-            
             get :index
-            
             expect(response).to render_template("index")
         end
     end
@@ -14,6 +12,13 @@ RSpec.describe WelcomeController, type: :controller do
         it "renders the about template" do
             get :about
             expect(response).to render_template("about")
+        end
+    end
+    
+        describe "GET faq" do
+        it "renders the faq template" do
+            get :faq
+            expect(response).to render_template("faq")
         end
     end
 end
