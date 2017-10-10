18e90f9a6d2fefaffa80ca0f749f0fc9b772e063
William Van Deren <wvanderen@gmail.com>
2017-10-10 13:30:44 +0000

    19a-2

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,20 +1,7 @@
 class AdvertisementsController < ApplicationController
   def index
-    @advertisements = Advertisement.all
   end
 
-  def show
-    @advertisement = Advertisement.find(params[:id])
-  end
-
-  def new
-    @advertisement = Advertisement.new
-  end
-
-  def create
-    @advertisement = Advertisement.new
-    @advertisement.title = params[:advertisement][:title]
-    @advertisement.body = params[:advertisement][:body]
-    @advertisement.price = params[:advertisement][:price]
+  def shownew
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'advertisements/index'
+
+  get 'advertisements/shownew'
+
   resources :advertisements
   
   resources :posts
