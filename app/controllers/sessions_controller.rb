2a904689524c5554516584b9f99b5604bebb4b06
William Van Deren <wvanderen@gmail.com>
2017-10-13 13:20:06 +0000

    27-1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,8 +20,4 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
     
-    def avatar_url(user)
-        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-        "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-    end
 end
