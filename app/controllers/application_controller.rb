dfc65d0f30eb0cacae40f9f10ec8b693ee19fd21
William Van Deren <wvanderen@gmail.com>
2017-10-13 14:00:37 +0000

    checkpoint27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  def avatar_url(user)
-        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-        "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  private 
+  def require_sign_in
+    unless current_user
+      flash[:alert] = "You must be logged in to do that"
+      redirect_to new_session_path
+    end
   end
+  
 end
