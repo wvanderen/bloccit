e09c2b27ba96025266eb8d1262fdddeb107f9979
William Van Deren <wvanderen@gmail.com>
2017-10-13 12:53:32 +0000

    checkpoint25-assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
