b620adfb38f2e87b8d1ba8e9f3c71e33843cd6a0
William Van Deren <wvanderen@gmail.com>
2017-10-16 15:03:14 +0000

    c30-1

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,15 @@ end
 topics = Topic.all
 
 50.times do
-    Post.create!(
+    post = Post.create!(
         user: users.sample,
         topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
         )
+        
+        post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+        rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample) }
 end
 
 
@@ -97,3 +100,4 @@ puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Advertisement.count} advertisements created"
 puts "#{Question.count} questions created"
+puts "#{Vote.count} votes created"
